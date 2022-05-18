885b8ea36741741f15f7a43e96fe7a57cf330c85
Muhammad Khuzain <mkhuzain17@gmail.com>
2022-05-18 22:20:54 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
+import React, { Fragment, useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  // jangan lupa untuk membuat data dan setter
+  let [hits, setHits] = useState([]);
+  let [user, setUser] = useState([]);
+  let [user1, setUser1] = useState([]);
+  let [post, setPost] = useState([]);
+
+  const fetchUser = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => setUser(data));
+  }
+
+  const fetchUser1 = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => setUser1(data));
+  }
+
+  useEffect(() => {
+    // cara 1, langsung tulis isi fetch di dalam useEffect
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(data => setPost(data));
+
+    fetch("https://hn.algolia.com/api/v1/search?query=foo&tags=story")
+      .then(response => response.json())
+      .then(data => setHits(data.hits))
+
+    // cara 2, buat function fetch, kemudia panggil di dalam useEffect
+    fetchUser();
+    fetchUser1();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <ul>
+        {user.map(item => (
+          <li key={item.id}>
+            <div>{item.id}</div>
+            <div>{item.name}</div>
+          </li>
+        ))}
+      </ul>
+
+      <ol>
+        {user1.map(item => (
+          <li key={item.id}>
+            <div>{item.name}</div>
+          </li>
+        ))}
+      </ol>
+
+      <ul>
+        {post.map(item => (
+          <li key={item.id}>
+            <div>{item.id}</div>
+            <div>{item.title}</div>
+            <div>{item.body}</div>
+          </li>
+        ))}
+      </ul>
+
+      <button type="button">
+        Search
+      </button>
+
+      <ul>
+        {hits.map(item => (
+          <li key={item.objectID}>
+            <div>{item.objectID}</div>
+            <div>{item.title}</div>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
